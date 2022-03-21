d35ee5555a0835e5579049d4ce3db7c31e1a4b4a
Jared Asmus <jaredutah@gmail.com>
2022-03-21 15:52:53 -0600

    Securing the API

--- src/create.js
+++ src/create.js
@@ -9,7 +9,7 @@ export const main = handler(async (event) => {
     TableName: process.env.TABLE_NAME,
     Item: {
       // The attributes of the item to be created
-      userId: '123', // The id of the author
+      userId: event.requestContext.authorizer.iam.cognitoIdentity.identityId, // The id of the author
       noteId: uuid.v1(), // A unique uuid
       content: data.content, // Parsed from request body
       attachment: data.attachment, // Parsed from request body
@@ -34,3 +34,28 @@ https://geuu1yrkik.execute-api.us-east-1.amazonaws.com/notes/eab2a810-a944-11ec-
 /*
 DELETE - curl -X DELETE https://5bv7x0iuga.execute-api.us-east-1.amazonaws.com/notes/NOTE_ID
 */
+
+aws cognito-idp sign-up \
+--region us-east-1 \
+--client-id 40mung33i54b3mb0r97ca3vija \
+--username admin@example.com \
+--password Passw0rd!
+
+aws cognito-idp admin-confirm-sign-up \
+--region us-east-1 \
+--user-pool-id us-east-1_hq9w9uxvd \
+--username admin@example.com
+
+
+npx aws-api-gateway-cli-test \
+--username='admin@example.com' \
+--password='Passw0rd!' \
+--user-pool-id='us-east-1_hq9w9uxvd' \
+--app-client-id='40mung33i54b3mb0r97ca3vija' \
+--cognito-region='us-east-1' \
+--identity-pool-id='us-east-1:90ef671f-2b0e-471f-905a-a9edeb5c8dd4' \
+--invoke-url='https://geuu1yrkik.execute-api.us-east-1.amazonaws.com' \
+--api-gateway-region='us-east-1' \
+--path-template='/notes' \
+--method='POST' \
+--body='{"content":"hello world","attachment":"hello.jpg"}'
